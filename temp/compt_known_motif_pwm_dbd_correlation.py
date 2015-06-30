6f7758c06a10c6f3c0791152b48a90f72aa0086d
dragontt <yimingkang588@gmail.com>
2015-06-30 11:49:26 -0500

    update pwm dbd correlation

--- temp/compt_known_motif_pwm_dbd_correlation.py
+++ temp/compt_known_motif_pwm_dbd_correlation.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python
 import os
+import sys
 import numpy
 import matplotlib.pyplot as plt
 from scipy.optimize import leastsq
 
-# species = "yeast"
-# species_full = "Saccharomyces_cerevisiae"
-species = "fly"
-species_full = "Drosophila_melanogaster"
-
-min_score_dbd = 10
-itv_score_dbd = 5
+# species = "yeast"; species_full = "Saccharomyces_cerevisiae"
+species = "fly"; species_full = "Drosophila_melanogaster"
+min_score_dbd = 0
+itv_score_dbd = 10
+# score_type = "median"
+score_type = "average"
+parse_method = "e_value"
+# parse_method = "-log10_fc"
+fit_function = "sigmoid"
+# fit_function = "exponential"
 
 def main():
-
 	# dir_proj = "/home/mblab/ykang/proj_db_infer_pipe/"
 	dir_proj = "/Users/KANG/cgscluster/proj_db_infer_pipe/"
 	fn_motif_list = dir_proj + "resources/cisbp_1.01/cisbp_motifs_" + species_full + ".txt"
@@ -29,48 +32,60 @@ def main():
 		fn_dbd = dir_dbd + motif
 		fn_pwm = dir_pwm + motif + ".tomtom"
 		if (os.path.isfile(fn_dbd) and os.path.getsize(fn_dbd) > 0) and (os.path.isfile(fn_pwm) and os.path.getsize(fn_pwm) > 0):
-			dict_dbd = parse_dict(fn_dbd, True, min_score_dbd)
-			dict_pwm = parse_dict(fn_pwm, False, None)
+			dict_dbd = parse_dict(fn_dbd, True, min_score_dbd, None)
+			dict_pwm = parse_dict(fn_pwm, False, None, parse_method)
 			if bool(dict_dbd) and bool(dict_pwm):
 				targets = numpy.intersect1d(dict_dbd.keys(), dict_pwm.keys())
 				targets_used = []
 				for target in targets:
 					if not target in targets_used:
 						scores_dbd.append(dict_dbd[target])
-						scores_pwm.append(1 - dict_pwm[target])
+						scores_pwm.append(dict_pwm[target])
 						targets_used.append(target)
 
 	# bin scores
 	[bins_scores_dbd, med_scores_pwm, avg_scores_pwm] = compt_bins(scores_dbd, scores_pwm, min_score_dbd, itv_score_dbd)	
-	y = med_scores_pwm
-	label_scatter = "median scores"
-	# y = avg_scores_pwm
-	# label_scatter = "average scores"
+	if score_type == 'median':
+		y = med_scores_pwm
+		label_scatter = "median scores"
+	elif score_type == 'average':
+		y = avg_scores_pwm
+		label_scatter = "average scores"
+	else:
+		sys.exit("Median or average score not specified")
 
 	# logistic fit
 	p_guess=(numpy.median(bins_scores_dbd), numpy.median(y), 1, .25)
-	p, cov, infodict, mesg, ier = leastsq(residuals, p_guess, args=(bins_scores_dbd, y), full_output=1)
-	# y = scores_pwm
-	# p_guess=(numpy.median(scores_dbd), numpy.median(y), 1, .25)
-	# p, cov, infodict, mesg, ier = leastsq(residuals, p_guess, args=(scores_dbd, y), full_output=1)
+	if fit_function == 'sigmoid':
+		p, cov, infodict, mesg, ier = leastsq(sigmoid_residuals, p_guess, args=(bins_scores_dbd, y), full_output=1)
+	elif fit_function == 'exponential':
+		p, cov, infodict, mesg, ier = leastsq(exponential_residuals, p_guess, args=(bins_scores_dbd, y), full_output=1)
+	else:
+		sys.exit("Fit function not specified")
 
 	# plot results
 	plt.figure()
 	plt.scatter(scores_dbd, scores_pwm, alpha=.5)
 	plt.scatter(bins_scores_dbd, y, alpha=.5, s=100, c='g', label=label_scatter)
 	xp = numpy.linspace(min_score_dbd, 100, 100)
-	pxp = sigmoid(p, xp)
-	label_fit = "sigmoid fit, c=" + str(p[2]) + ", k=" + str(p[3])
+	if fit_function == 'sigmoid':
+		pxp = sigmoid(p, xp)
+		label_fit = "sigmoid fit, c=" + str(p[2]) + ", k=" + str(p[3])
+	elif fit_function == 'exponential':
+		pxp = exponential(p, xp)
+		label_fit = "exponential fit, c=" + str(p[2]) + ", k=" + str(p[3])
+	else:
+		sys.exit("Fit function not specified")
 	plt.plot(xp, pxp, 'r-', label=label_fit)
 	plt.legend(loc="lower right")
 	plt.xlim([0, 100])
-	# plt.ylim([.5, 1])
+	plt.ylim([0, 1])
 	plt.title("DBD vs PWM Similarity, Species: " + species)
 	plt.xlabel("DBD Percent Identity")
 	plt.ylabel("PWM Similarity (1 - E_value)")
 	plt.show()
 
-def parse_dict(fn, use_cutoff, min_score_dbd):
+def parse_dict(fn, use_cutoff, min_score_dbd, parse_method):
 	lines = open(fn, 'r').readlines()
 	dict = {}
 	if len(lines) > 1:
@@ -82,11 +97,19 @@ def parse_dict(fn, use_cutoff, min_score_dbd):
 					break
 				dict[target] = score
 		else:
-			baseline = float(lines[0].split()[1])
-			for i in range(len(lines)):
-				target = lines[i].split()[0].split(":")[0]
-				score = float(lines[i].split()[1])
-				dict[target] = float(score)
+			if parse_method == 'e_value':
+				for i in range(1, len(lines)):
+					target = lines[i].split()[0].split(":")[0]
+					score = float(lines[i].split()[1])
+					dict[target] = 1 - score
+			elif parse_method == '-log10_fc':
+				baseline = -numpy.log10(float(lines[0].split()[1]))
+				for i in range(1, len(lines)):
+					target = lines[i].split()[0].split(":")[0]
+					score = -numpy.log10(float(lines[i].split()[1]))
+					dict[target] = score/baseline
+			else:
+				sys.exit("PWM parse method not specified")
 	return dict
 
 def compt_bins(scores_dbd, scores_pwm, min_score_dbd, itv_score_dbd):
@@ -105,11 +128,19 @@ def compt_bins(scores_dbd, scores_pwm, min_score_dbd, itv_score_dbd):
 
 def sigmoid(p,x):
     x0,y0,c,k = p
-    y = c / (1 + numpy.exp(-k*(x-x0))) + y0
+    y = c / (1 + numpy.exp(-k * (x-x0))) + y0
     return y
 
-def residuals(p,x,y):
-    return y - sigmoid(p,x)
+def sigmoid_residuals(p,x,y):
+	return y - sigmoid(p, x)
+
+def exponential(p,x):
+	x0,y0,c,k = p
+	y = c * numpy.exp(k * (x-x0)) + y0
+	return y
+
+def exponential_residuals(p,x,y):
+	return y - exponential(p, x)
 
 if __name__ == "__main__":
     main()
--- temp/eval_cellcycle+motif.py
+++ temp/eval_cellcycle+motif.py
@@ -38,6 +38,8 @@ def main(argv):
     dir_sub = 'cisbp_-2000_+200_fimo_known_motif/analysis_flynet_top4to40k/'
     fns.append(dir_network + dir_sub + 'analysis_chip_support.combined_network_np_motif_net_known_motif_resort_0.txt')
     fns.append(dir_network + dir_sub + 'analysis_pwm_support.combined_network_np_motif_net_known_motif_resort_0.txt')
+    fns.append(dir_network + dir_sub + 'analysis_chip_support.combined_network_np_motif_net_known_motif_quantcomb.txt')
+    fns.append(dir_network + dir_sub + 'analysis_pwm_support.combined_network_np_motif_net_known_motif_quantcomb.txt')
 
     dir_network = '/Users/KANG/cgscluster/proj_db_infer_pipe/output/fly_network_cellCycle_motif_incorporated/'
     dir_sub = 'cisbp_-2000_+200_fimo_dbd_cutoff_cellCycle_np/analysis_flynet_top4to40k/'
@@ -64,11 +66,12 @@ def main(argv):
     fns.append(dir_network + dir_sub + 'analysis_pwm_support.combined_network_np_motif_net_dbd_cutoff_60.0_quantcomb.txt')
 
     # figure setup
-    colors = ['k:', 'k', 'm', 'r:', 'g:', 'b:', 'r--', 'g--', 'b--', 'r-.', 'g-.', 'b-.']
+    colors = ['k:', 'k', 'm', 'm--', 'r:', 'g:', 'b:', 'r--', 'g--', 'b--', 'r-.', 'g-.', 'b-.']
     labels = []
     labels.append('chance')
     labels.append('np')
-    labels.append('np + cisbp_known_motif')
+    labels.append('np + cisbp_known_motif_resort')
+    labels.append('np + cisbp_known_motif_quantcomb')
     labels.append('np + motif_cutoff_40_geomean')
     labels.append('np + motif_cutoff_50_geomean')
     labels.append('np + motif_cutoff_60_geomean')
