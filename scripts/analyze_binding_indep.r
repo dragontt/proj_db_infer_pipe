c7c6e206b23973fa81c61a0e87679a4b38e79207
yimingkang588@gmail.com <KANG@Yimings-MBP.cgs.wustl.edu>
2015-07-07 15:50:55 -0500

    modify network analysis filename

--- scripts/analyze_binding_indep.r
+++ scripts/analyze_binding_indep.r
@@ -74,8 +74,10 @@ for (np_conf_cutoff in np_conf_cutoffs)
 	}
 }
 
-# filename <- paste("chip.bp.np.set.sizes.top2to20k.",strsplit(network,"[./]")[[1]][4],".txt", sep="")
-filename <- paste("chip.bp.np.set.sizes.top4to40k.",strsplit(network,"[./]")[[1]][4],".txt", sep="")
-# filename <- paste("chip.bp.np.set.sizes.top20to200k.",strsplit(network,"[./]")[[1]][4],".txt", sep="")
-write.table(cbind(chip.bp.np.setsizes, rep(interaction_bsinfo_cnt, length=nrow(chip.bp.np.setsizes)), rep(interaction_pdna.inter_cnt, length=nrow(chip.bp.np.setsizes))),file=filename,col.names=FALSE,row.names=FALSE,quote=FALSE,sep='\t')
+tempNameList <- strsplit(network,"[./]")[[1]]
+tempBasename <- paste(tempNameList[4:(length(tempNameList)-1)],collapse=".")
+# fnOutName <- paste("chip.bp.np.set.sizes.top2to20k.",tempBasename,".txt", sep="")
+fnOutName <- paste("chip.bp.np.set.sizes.top4to40k.",tempBasename,".txt", sep="")
+# fnOutName <- paste("chip.bp.np.set.sizes.top20to200k.",tempBasename,".txt", sep="")
+write.table(cbind(chip.bp.np.setsizes, rep(interaction_bsinfo_cnt, length=nrow(chip.bp.np.setsizes)), rep(interaction_pdna.inter_cnt, length=nrow(chip.bp.np.setsizes))),file=fnOutName,col.names=FALSE,row.names=FALSE,quote=FALSE,sep='\t')
 
