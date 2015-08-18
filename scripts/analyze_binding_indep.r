5b5fad3eabe99bcd7e880bef7d47b73a1673bff0
Your Name <you@example.com>
2015-08-18 14:19:41 -0500

    fix independent net eval

--- scripts/analyze_binding_indep.r
+++ scripts/analyze_binding_indep.r
@@ -3,6 +3,9 @@ network <- toString(args[1])
 chip_net <- toString(args[2])
 pwm_net <- toString(args[3])
 
+maxRank <- 40000
+fnRankCaption <- "chip.bp.np.set.sizes.top4to40k."
+
 net <- read.table(network,header=TRUE)
 pdna.inter <- read.table(chip_net,header=TRUE)
 bsinfo <- read.table(pwm_net,header=TRUE)
@@ -17,9 +20,8 @@ reg_bsinfo <- intersect(intersect(unique(as.character(bsinfo$REGULATOR)),unique(
 interaction_pdna.inter_cnt <- length(reg_pdna.inter) * length(orf_universe)
 interaction_bsinfo_cnt <- length(reg_bsinfo) * length(orf_universe)
 
-# np_conf_cutoffs <- net$CONFIDENCE[order(net$CONFIDENCE,decreasing=TRUE)][seq(2000,20000,by=2000)]
-np_conf_cutoffs <- net$CONFIDENCE[order(net$CONFIDENCE,decreasing=TRUE)][seq(4000,40000,by=4000)]
-# np_conf_cutoffs <- net$CONFIDENCE[order(net$CONFIDENCE,decreasing=TRUE)][seq(20000,200000,by=20000)]
+np_conf_cutoffs <- net$CONFIDENCE[order(net$CONFIDENCE,decreasing=TRUE)][seq(maxRank/10,maxRank,by=maxRank/10)]
+
 for (np_conf_cutoff in np_conf_cutoffs)
 {
 	for (chip_desired_recall in c(0.1)) # c(0.1,0.33,0.5)
@@ -48,7 +50,7 @@ for (np_conf_cutoff in np_conf_cutoffs)
 				
 				binding.evid.max[match(as.character(bsinfo$TARGET[which(bsinfo$REGULATOR==regulator)]),targets)] <- bsinfo$MAXP[which(bsinfo$REGULATOR==regulator)]
 				binding.evid.sum[match(as.character(bsinfo$TARGET[which(bsinfo$REGULATOR==regulator)]),targets)] <- bsinfo$SUMP[which(bsinfo$REGULATOR==regulator)]
-				cat(regulator,length(targets),"\n")
+				# cat(regulator,length(targets),"\n")
 				binding.evid.comb <- apply(rbind(binding.evid.max,binding.evid.sum),2,max)	
 				prc.comb <- compute.prc(binding.evid.comb,pdna.evid)
 			
@@ -68,7 +70,6 @@ for (np_conf_cutoff in np_conf_cutoffs)
 			}
 			i <- i + 1
 		}
-		
 		chip.bp.np.setsize <- apply(chip.bp.np.sets,2,sum)
 		chip.bp.np.setsizes <- rbind(chip.bp.np.setsizes,chip.bp.np.setsize)
 	}
@@ -76,8 +77,6 @@ for (np_conf_cutoff in np_conf_cutoffs)
 
 tempNameList <- strsplit(network,"[./]")[[1]]
 tempBasename <- paste(tempNameList[4:(length(tempNameList)-2)],collapse=".")
-# fnOutName <- paste("chip.bp.np.set.sizes.top2to20k.",tempBasename,".txt", sep="")
-fnOutName <- paste("chip.bp.np.set.sizes.top4to40k.",tempBasename,".txt", sep="")
-# fnOutName <- paste("chip.bp.np.set.sizes.top20to200k.",tempBasename,".txt", sep="")
+fnOutName <- paste(fnRankCaption,tempBasename,".txt", sep="")
 write.table(cbind(chip.bp.np.setsizes, rep(interaction_bsinfo_cnt, length=nrow(chip.bp.np.setsizes)), rep(interaction_pdna.inter_cnt, length=nrow(chip.bp.np.setsizes))),file=fnOutName,col.names=FALSE,row.names=FALSE,quote=FALSE,sep='\t')
 
--- scripts/analyze_network.r
+++ scripts/analyze_network.r
@@ -6,55 +6,57 @@ fnGoldNetwork <- toString(args[4])
 fnOutName <- toString(args[5])
 maxRank <- as.integer(args[6])*1000
 
-# rank_cutoffs <- seq(2000, 20000, 2000)
-# rank_cutoffs <- seq(4000, 40000, 4000)
-# rank_cutoffs <- seq(20000, 200000, 20000)
 rank_cutoffs <- seq(maxRank/10, maxRank, maxRank/10)
 
-predictedNetwork <- as.matrix(read.table(fnPredictedNetwork, header=TRUE))
-goldNetwork <- as.matrix(read.table(fnGoldNetwork))
+predictedNetwork <- as.matrix(read.table(fnPredictedNetwork, header=FALSE))
+goldNetwork <- as.matrix(read.table(fnGoldNetwork), header=FALSE)
 tfs <- as.matrix(read.table(fnTfs))
 genes <- as.matrix(read.table(fnGenes))
 
 evaluateEvidSupport <- function(predictedNetwork, goldNetwork, tfs, genes, rank_cutoffs) {
-        goldNetwork <- filter_auto(goldNetwork, tfs, genes)
-        # predictedNetwork <- filter_auto(predictedNetwork, tfs, genes)
+    goldNetwork <- filter_auto(goldNetwork, tfs, genes)
+    # predictedNetwork <- filter_auto(predictedNetwork, tfs, genes)
 
-        gold_inds <- which(apply(goldNetwork,1,max)>0)
-        # cat(gold_inds, "\n")
-        res <- matrix(nrow=4, ncol=length(rank_cutoffs), data=0)
-        res[1,] <- rep(length(goldNetwork[gold_inds,]), length(rank_cutoffs))
-        res[2,] <- rep(sum(goldNetwork[gold_inds,]), length(rank_cutoffs))
+    gold_inds <- which(apply(goldNetwork,1,max)>0)
 
-        # evaluate all tf edges
-        cutoffs <- abs(predictedNetwork)[order(abs(predictedNetwork), decreasing=TRUE)][rank_cutoffs]
-        for(j in 1:length(cutoffs)) {
-                inds <- which(abs(predictedNetwork)[gold_inds,] >= cutoffs[j])
-                res[3,j] <- length(inds)
-                res[4,j] <- sum(goldNetwork[gold_inds,][inds])
+    res <- matrix(nrow=4, ncol=length(rank_cutoffs), data=0)
+    res[1,] <- rep(length(goldNetwork[gold_inds,]), length(rank_cutoffs))
+    res[2,] <- rep(sum(goldNetwork[gold_inds,]), length(rank_cutoffs))
+
+    # evaluate all tf edges
+    cutoffs <- abs(predictedNetwork)[order(abs(predictedNetwork), decreasing=TRUE)][rank_cutoffs]
+
+    for(j in 1:length(cutoffs)) {
+        for (gold_ind in gold_inds) {
+            target_inds <- which(abs(predictedNetwork[gold_ind,]) > cutoffs[j])
+            res[3,j] <- res[3,j] + length(target_inds)
+            res[4,j] <- res[4,j] + sum(goldNetwork[gold_ind,][target_inds])
         }
+        # predictedSubNet <- abs(predictedNetwork)[gold_inds,]
+        # inds <- which(predictedSubNet > cutoffs[j])
+        # res[3,j] <- length(inds)
+        # res[4,j] <- sum(goldNetwork[inds])
+    }
 
-        # # evaluate chip tf edges
-        # cutoffs <- abs(predictedNetwork[gold_inds,])[order(abs(predictedNetwork[gold_inds,]), decreasing=TRUE)][rank_cutoffs]
-        # for(j in 1:length(cutoffs)) {
-        #         if(j == 1) {
-        #                 inds <- which(abs(predictedNetwork)[gold_inds,] >= cutoffs[j])
-        #         } else {
-        #                 inds <- which(abs(predictedNetwork)[gold_inds,] >= cutoffs[j] & abs(predictedNetwork)[gold_inds,] < cutoffs[j-1])
-        #         }
-        #         res[3,j] <- sum(goldNetwork[gold_inds,][inds])/length(inds)
-        # }
+    # # evaluate chip tf edges
+    # cutoffs <- abs(predictedNetwork[gold_inds,])[order(abs(predictedNetwork[gold_inds,]), decreasing=TRUE)][rank_cutoffs]
+    # for(j in 1:length(cutoffs)) {
+    #     if(j == 1) {
+    #             inds <- which(abs(predictedNetwork)[gold_inds,] >= cutoffs[j])
+    #     } else {
+    #             inds <- which(abs(predictedNetwork)[gold_inds,] >= cutoffs[j] & abs(predictedNetwork)[gold_inds,] < cutoffs[j-1])
+    #     }
+    #     res[3,j] <- sum(goldNetwork[gold_inds,][inds])/length(inds)
+    # }
 
-        write.table(res, file=fnOutName, col.names=FALSE, row.names=FALSE, quote=FALSE, sep='\t')
+    write.table(res, file=fnOutName, col.names=FALSE, row.names=FALSE, quote=FALSE, sep='\t')
 }
 
 filter_auto <- function(network, tfs, genes){
-        select <- cbind(1:length(tfs), match(tfs,genes))
-        select <- select[which(!is.na(select[,2])),]
-        network[select] <- 0
-        network
+    select <- cbind(1:length(tfs), match(tfs,genes))
+    select <- select[which(!is.na(select[,2])),]
+    network[select] <- 0
+    network
 }
 
 evaluateEvidSupport(predictedNetwork, goldNetwork, tfs, genes, rank_cutoffs)
-
-# evaluateEvidSupport(predictedNetwork, goldNetwork[match(intersect(combine_tfs, tfs), tfs), match(intersect(combine_genes, genes), genes)], combine_tfs, combine_genes, seq(2000, 20000, 2000))
